edd6606c5aee1aec786856ae85b31afa6575adc3
Frank Matranga <thefrankmatranga@gmail.com>
2020-05-08 00:28:56 -0400

    Book card with log dialog

--- lib/common/book_card.dart
+++ lib/common/book_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:libellum/models/books.dart';
 
 class BookCard extends StatelessWidget {
@@ -6,26 +7,83 @@ class BookCard extends StatelessWidget {
 
   final Book book;
 
+  void _showLogDialog(context, bookPageCount) {
+    showDialog(
+        context: context,
+        child: AlertDialog(
+          title: Text('What page are you up to now?'),
+          content: TextField(
+            autofocus: true,
+            inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
+            keyboardType: TextInputType.number,
+            decoration: new InputDecoration(
+                labelText: 'Page Number', hintText: 'max $bookPageCount'),
+          ),
+          actions: <Widget>[
+            OutlineButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Set'),
+            )
+          ],
+        ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    var w600 = FontWeight.w600;
     return Card(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Image.asset('assets/images/book_cover.jpg'),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 20),
-            child: Text(book.title, style: TextStyle(fontSize: 30, fontWeight: w600),),
+          Ink.image(
+            image: AssetImage('assets/images/book_cover.jpg'),
+            fit: BoxFit.cover,
+            width: 100,
+            height: 150.0,
+            child: InkWell(
+              onTap: () {
+                Navigator.pushNamed(context, '/books/show', arguments: book);
+              },
+            ),
           ),
-          ButtonBar(
-            children: <Widget>[
-              FlatButton(
-                child: Text('Read'),
-                onPressed: () {},
-              )
-            ],
-          )
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.only(left: 10, top: 10, right: 10),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    book.title,
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                  Text("You've read - of ${book.pageCount} pages"),
+                  ButtonBar(
+                    children: <Widget>[
+                      FlatButton.icon(
+                          onPressed: () {
+                            _showLogDialog(context, book.pageCount);
+                          },
+                          icon: Icon(Icons.check),
+                          label: Text('Log'),
+                          color: Colors.green),
+                      FlatButton.icon(
+                        onPressed: () {},
+                        icon: Icon(Icons.remove),
+                        label: Text('Not Today'),
+                        color: Colors.red,
+                      )
+                    ],
+                  ),
+                  Divider(),
+                  LinearProgressIndicator(value: 0.4),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
--- lib/screens/add_book.dart
+++ lib/screens/add_book.dart
@@ -52,7 +52,6 @@ class _AddBookFormState extends State<AddBookForm> {
             },
             child: Text('Submit'),
           )
-          // Add TextFormFields and RaisedButton here.
         ]));
   }
 }
